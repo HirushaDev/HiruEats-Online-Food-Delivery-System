46319f1619124e63eab8b37accf9728321e64830
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-05-29 09:55:06 +0530

    Add backend folder and initial backend setup

---
+++ backend/src/test/java/in/hirueats_online_food_delivery_system/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hirueats_online_food_delivery_system.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
