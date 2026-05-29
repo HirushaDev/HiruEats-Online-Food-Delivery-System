ea548365a76d200f98abcc94082e7e2f39fde6fb
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-05-29 10:36:05 +0530

    implement ProfileService for user profile creation

---
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Service/ProfileService.java
@@ -0,0 +1,9 @@
+package in.hirueats_online_food_delivery_system.backend.Service;
+
+import in.hirueats_online_food_delivery_system.backend.IO.ProfileRequest;
+import in.hirueats_online_food_delivery_system.backend.IO.ProfileResponse;
+
+public interface ProfileService {
+
+      ProfileResponse createProfile(ProfileRequest request);
+}
---
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Service/ProfileServiceImpl.java
@@ -0,0 +1,52 @@
+package in.hirueats_online_food_delivery_system.backend.Service;
+
+import in.hirueats_online_food_delivery_system.backend.Entity.UserEntity;
+import in.hirueats_online_food_delivery_system.backend.IO.ProfileRequest;
+import in.hirueats_online_food_delivery_system.backend.IO.ProfileResponse;
+import in.hirueats_online_food_delivery_system.backend.Repostory.UserRepostory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileServiceImpl implements  ProfileService{
+
+      private final UserRepostory userRepostory;
+
+
+    @Override
+    public ProfileResponse createProfile(ProfileRequest request) {
+
+        UserEntity newProfile = convertToUserEntity(request);
+        newProfile = userRepostory.save(newProfile);
+        return convertToProfileResponse(newProfile);
+
+    }
+
+    private ProfileResponse convertToProfileResponse(UserEntity newProfile) {
+       return ProfileResponse.builder()
+                .name(newProfile.getName())
+                .email(newProfile.getEmail())
+                .userId(newProfile.getUserId())
+                .isAccountVerified(newProfile.getIsAccountVerified())
+                .build();
+
+    }
+
+    private UserEntity convertToUserEntity(ProfileRequest request) {
+      return  UserEntity.builder()
+                .email(request.getEmail())
+                .userId(UUID.randomUUID().toString())
+                .name(request.getName())
+                .password(request.getPassword())
+                .isAccountVerified(false)
+                .resetOtpExpiredAt(0L)
+                .verifyOtp(null)
+                .verifyOtpExpireAt(0L)
+                .resetOtp(null)
+                .build();
+
+    }
+}
