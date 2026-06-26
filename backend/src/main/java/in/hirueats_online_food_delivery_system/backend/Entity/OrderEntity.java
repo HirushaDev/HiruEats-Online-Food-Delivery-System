8562901bdbaa6195bea60078e27cfc97c3e96231
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 19:07:23 +0530

    create OrderEntity with JPA mappings

--- backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Entity/OrderEntity.java
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Entity/OrderEntity.java
@@ -2,27 +2,36 @@ package in.hirueats_online_food_delivery_system.backend.Entity;
 
 
 import in.hirueats_online_food_delivery_system.backend.IO.OrderItem;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Entity
 @Data
 @Builder
 @Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEntity {
     @Id
-      private String Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long Id;
+
       private String userId;
       private String userAddress;
-      private String PhoneNumber;
+      private String phoneNumber;
       private String email;
-      private List<OrderItem> orderItemList;
       private double amount;
       private String paymentStatus;
       private String orderStatus;
-      private String razorpayOrderId;
-      private String razorpaySignature;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
+      private List<OrderItem> orderItemList;
+
+
 }
