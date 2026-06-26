53917ad0a5aa2a8b906199931c8b8d20c184f1c1
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 15:13:30 +0530

    implement AppUserDetailsService for Spring Security

--- backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Service/AppUserDetailsService.java
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Service/AppUserDetailsService.java
@@ -3,24 +3,41 @@ package in.hirueats_online_food_delivery_system.backend.Service;
 import in.hirueats_online_food_delivery_system.backend.Entity.UserEntity;
 import in.hirueats_online_food_delivery_system.backend.Repostory.UserRepostory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AppUserDetailsService implements UserDetailsService {
 
-      private final UserRepostory userRepostory;
+    private final UserRepostory userRepostory;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-         UserEntity existingUser =  userRepostory.findByEmail(email)
-                   .orElseThrow(()-> new UsernameNotFoundException("Email not found "+email));
-         return new User(existingUser.getEmail(), existingUser.getPassword(), new ArrayList<>());
+
+        UserEntity existingUser = userRepostory.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Email not found " + email)
+                );
+
+        boolean isEnabled = existingUser.getIsAccountDisabled() == null || !existingUser.getIsAccountDisabled();
+
+        return new User(
+                existingUser.getEmail(),
+                existingUser.getPassword(),
+                isEnabled,
+                true,
+                true,
+                true,
+                List.of(
+                        new SimpleGrantedAuthority("ROLE_" + existingUser.getRole())
+                )
+        );
     }
 }
