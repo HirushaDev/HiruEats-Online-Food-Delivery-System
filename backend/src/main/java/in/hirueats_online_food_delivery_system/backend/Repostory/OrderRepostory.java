dd623fb14fc69f8619b9c3d0661adfbba8482f8b
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 17:48:25 +0530

    add findByRazorpayOrderId to OrderRepository

--- backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Repostory/OrderRepostory.java
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Repostory/OrderRepostory.java
@@ -4,12 +4,15 @@ import in.hirueats_online_food_delivery_system.backend.Entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface OrderRepostory extends JpaRepository<OrderEntity, String> {
 
     List<OrderEntity> findByUserId(String userId);
 
-    
+    Optional<OrderEntity> findByRazorpayOrderId(String  razorpayOrderId);
+
+
 
 }
