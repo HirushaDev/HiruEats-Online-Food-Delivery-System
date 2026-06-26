f1b230064edd31e55d0c1f471b720633e16f6fb0
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 19:10:00 +0530

    create OrderRepository for order data access

--- backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Repostory/OrderRepostory.java
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Repostory/OrderRepostory.java
@@ -4,14 +4,14 @@ import in.hirueats_online_food_delivery_system.backend.Entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 
-public interface OrderRepostory extends JpaRepository<OrderEntity, String> {
+
+public interface OrderRepostory extends JpaRepository<OrderEntity, Long> {
 
     List<OrderEntity> findByUserId(String userId);
 
-    Optional<OrderEntity> findByRazorpayOrderId(String  razorpayOrderId);
+
 
 
 
