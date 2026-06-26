682026347c8406976f345d912741ce8fde6c457e
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 14:34:47 +0530

    create UserHome page with integrated components

--- client/src/Pages/UserHome.jsx
+++ client/src/Pages/UserHome.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import PrivateNavbar from '../Components/PrivateNavbar';
+import UserHeader from './UserHeader';
+import Explore from './explore';
+import FoodDisplay from './FoodDisplay';
+import JuiceDisplay from './JuiceDisplay';
 
 const UserHome = () => {
   return (
     <div>
-        <PrivateNavbar />
+       <UserHeader/>
+      
+       <Explore/>
+      
+       <div id="food-display">
+         <FoodDisplay/>
+       </div>
+        <hr className="explore-menu-hr" />
+       <div id="juice-display">
+         <JuiceDisplay/>
+       </div>
     </div>
   );
 }
