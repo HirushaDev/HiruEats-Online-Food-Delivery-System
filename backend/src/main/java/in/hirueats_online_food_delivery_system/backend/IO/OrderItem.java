a6f382574444f32d3e39dc9eb62a5fe263f91d4b
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 19:08:13 +0530

    convert OrderItem to JPA entity with database mapping

--- backend/src/main/java/in/hirueats_online_food_delivery_system/backend/IO/OrderItem.java
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/IO/OrderItem.java
@@ -1,11 +1,22 @@
 package in.hirueats_online_food_delivery_system.backend.IO;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItem {
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
       private String foodId;
       private String juiceId;
       private double price;
