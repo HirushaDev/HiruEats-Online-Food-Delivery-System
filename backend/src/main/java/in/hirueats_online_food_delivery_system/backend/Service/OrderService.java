42e18fdb4b4d24dccb72a87d5708642adfee5785
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 19:39:32 +0530

    add getUserOrders method to OrderService

--- backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Service/OrderService.java
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Service/OrderService.java
@@ -3,7 +3,13 @@ package in.hirueats_online_food_delivery_system.backend.Service;
 import in.hirueats_online_food_delivery_system.backend.IO.OrderRequest;
 import in.hirueats_online_food_delivery_system.backend.IO.OrderResponse;
 
+import java.util.List;
+
 public interface OrderService {
 
   OrderResponse createOrderWithPayment(OrderRequest request);
+
+  List<OrderResponse> getUserOrders(String userId);
+
+
 }
