e27f3327b14f6fcb4c99b0faad79a22659eb72cb
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-26 14:36:52 +0530

    implement AppContext for global state management

--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -1,6 +1,8 @@
 import { createContext } from 'react';
 import { useState } from 'react';
 import { AppConstants } from '../Util/constants';
+import { useEffect } from 'react';
+
 
 export const AppContext = createContext();
 
@@ -12,6 +14,38 @@ export const AppProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userData, setUserData] = useState(null);
     const [isEmailVerified, setIsEmailVerified] = useState(false);
+     const [cart, setCart] = useState({}); 
+   
+  const addToCart = (itemId) => {
+    if (!itemId) return;
+    setCart((prev) => ({
+      ...prev,
+      [itemId]: (prev[itemId] || 0) + 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    if (!itemId) return;
+    setCart((prev) => ({
+      ...prev,
+      [itemId]: Math.max(0, (prev[itemId] || 0) - 1),
+    }));
+  };
+
+  const deleteFromCart = (itemId) => {
+    if (!itemId) return;
+    setCart((prev) => {
+      const newCart = { ...prev };
+      delete newCart[itemId];
+      return newCart;
+    });
+  };
+
+  const cartCount = Object.values(cart).reduce(
+    (total, qty) => total + qty,
+    0,
+  );
+
 
     const contextValue = {
         BACKEND_URL,
@@ -20,8 +54,15 @@ export const AppProvider = ({ children }) => {
         userData,
         setUserData,
         isEmailVerified,
-        setIsEmailVerified
+        setIsEmailVerified,
+        cart,
+        addToCart,
+        removeFromCart,
+        deleteFromCart,
+        cartCount
     };
+
+
 
      return (
         <AppContext.Provider value={contextValue}>
