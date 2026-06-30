2a65deee030d980f78a731d26169ccdead62fd58
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-06-30 14:24:36 +0530

    add ResourceNotFoundException for contact service

---
+++ backend/src/main/java/in/hirueats_online_food_delivery_system/backend/Exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package in.hirueats_online_food_delivery_system.backend.Exception;
+
+public class ResourceNotFoundException extends  RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
